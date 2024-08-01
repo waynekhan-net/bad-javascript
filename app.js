548039e2ceea02efff2e6d669c7d2b1498c8353b
Wayne Khan <hello@waynekhan.net>
2024-08-01 13:53:24 +0800

    Initial gen

---
+++ app.js
@@ -0,0 +1,14 @@
+// Import the necessary modules
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
