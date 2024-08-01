859d2080b3cdf5a59cfc2b0b40ecf176bba0042b
Wayne Khan <hello@waynekhan.net>
2024-08-01 13:54:42 +0800

    Add /time

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Define a new route to get the current server time
+app.get('/time', (req, res) => {
+  const currentTime = new Date().toLocaleString();
+  res.send(`Current server time is: ${currentTime}`);
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
